640e2bcd05cc2c9551f9faa68f84682407878b1d
Lene Svit <lene261r@edu.ucl.dk>
2021-09-19 10:40:40 +0200

    organize.Busniess and Username

--- Organize.WASM/Pages/SignInBase.cs
+++ Organize.WASM/Pages/SignInBase.cs
@@ -6,23 +6,45 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using Organize.Shared.Enitites;
+using Organize.Business;
 
 namespace Organize.WASM.Pages
 {
     public class SignInBase: SignBase
     {
         protected string Day { get; set; } = DateTime.Now.DayOfWeek.ToString();
-        protected string Username { get; set; } = "Lene";
 
-        protected void HandleUserNameChanged(ChangeEventArgs eventArgs)
-        {
-            Username = eventArgs.Value.ToString();
-        }
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
 
-        protected void HandleUserNameValueChanged(string value)
+        protected  async void Onsubmit()
         {
-            Username = value;
+            if(EditContext.Validate())
+            {
+                return;
+            }
+
+            var userManager = new UserManager();
+            var foundUser =  await userManager.TrySignInAndGetUserAsync(User);
+
+            if(foundUser != null)
+            {
+                NavigationManager.NavigateTo("items");
+            }
+
+
         }
+        //protected string Username { get; set; } = "Lene";
+
+        //protected void HandleUserNameChanged(ChangeEventArgs eventArgs)
+        //{
+        //    Username = eventArgs.Value.ToString();
+        //}
+
+        //protected void HandleUserNameValueChanged(string value)
+        //{
+        //    Username = value;
+        //}
 
   
     }
